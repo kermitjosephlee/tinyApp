2ca9f46ed46b88b58b3877447daed0301e6afdcb
Kermit Joseph Lee <kermitjosephlee@gmail.com>
2018-07-13 13:23:54 -0400

    commented out the crap out of the code to get a better idea of what was going on. took out lines of dead code added in a panic to see if it worked.

--- express_server.js
+++ express_server.js
@@ -69,8 +69,6 @@ app.use(function (req, res, next) {
   console.log('- - - - - - - - - - - - - -');
   console.log("urlDB:", urlDatabase);
   console.log("- - - - - - - - - - - - - -");
-  console.log("loginValidator: ", loginValidator(req.body.email));
-  console.log("- - - - - - - - - - - - - -");
   console.log('########################  END  ############');
   next();
 });
@@ -83,7 +81,7 @@ app.use(function (req, res, next) {
 app.get("/", (req, res) => {
   let userID = req.cookies.user_id;
   let user = users[userID];
-  let templateVars = { urls: urlDatabase, user: user };
+  let templateVars = { urlDatabase: urlDatabase, user: user };
   res.render("url_index", templateVars)
 });
 
@@ -93,29 +91,43 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   let userID = req.cookies.user_id
   let user = users[userID];
-  let templateVars = { urls: urlDatabase, user: user };
+  let templateVars = { urlDatabase: urlDatabase, user: user };
   res.render("url_index", templateVars)
 });
 
-//
+// user is defined the "user_id" in the req.cookies within object users
+// templateVars is the key value pairs of variables passed to EJS
+// res.render is the call to express to send "url_login.ejs" with the data contained in obj templateVars
+// app.get is the express function call that accepts parameters "/login", and req (request) and res (result)
 app.get("/login", (req, res) => {
   let user = users[req.cookies["user_id"]];
-  let templateVars = { urls: urlDatabase, user: user };
+  let templateVars = { urlDatabase: urlDatabase, user: user };
   res.render("url_login", templateVars)
 })
 
 
+// get takes user which we define as name "user_id" from req.cookies from object users
+// we create an object called templateVar which EJS will see as the object of urlDatabase (left side) which we assign the value of object urlDatabase (right side)
+// express then renders "url_new" with this information and renders it at location /urls/new
 app.get("/urls/new", (req, res) => {
   let user = users[req.cookies["user_id"]];
-  let templateVars = { urls: urlDatabase, user: user };
+  let templateVars = { urlDatabase: urlDatabase, user: user };
   res.render("url_new", templateVars);
 });
 
 app.get("/register", (req, res) => {
   let user = users[req.cookies["user_id"]]
-  let templateVars = { urls: urlDatabase, user: user };
+  let templateVars = { urlDatabase: urlDatabase, user: user };
   res.render("url_register", templateVars)
 });
+
+
+// the app.post function call to register is designed to check to see if there are values in req.body.email and req.body.password and if the && boolean is satisfied, it will call on the generateRandomString function to return a randomized string.
+// we then define a new object called userObj defined as below (id: ,email:, password:, )
+// we then assign to the users object with a id key as the randomId with the userObj object
+// we then redirect the page to /urls
+
+// if the && boolean returns false, the page is redirected to the error page
 
 app.post("/register", (req, res) => {
 
@@ -132,6 +144,13 @@ app.post("/register", (req, res) => {
 
 });
 
+
+// when post login route is executed, first for testing purposes it will console.log that req.body.email has received the email string from the page.
+// we then define a variable called foundUser which will store the results of the calling of the function loginValidator. To start loginValidator, it will need the email and the password entered into the page
+// to check to see if there is an email and password combination in the users object, we check to see first if there are non-zero entries for req.body.email and req.body.password. We also check to see if foundUser has returned with a positive result from checking the email and password against the users object.
+// if all of these are satisfied, we create a cookie called user_id which will retain the randomized string id value, referenced as foundUser.id
+// we then redirect the page back to root
+// if the logic gate is not satisfied, we redirect to error
 app.post("/login", (req, res) => {
   console.log('req.body.email 2', req.body.email)
   let foundUser = loginValidator(req.body.email, req.body.password);
@@ -145,27 +164,29 @@ app.post("/login", (req, res) => {
 
 });
 
+// when post logout is called, we call on the clearCookie function to clear cookie with name user_id
+// we then redirect to root
 app.post("/logout", (req, res) => {
   res.clearCookie("user_id");
-  res.redirect("/urls");
+  res.redirect("/");
 })
 
+// post urls starts with a local variable tempStr be defined as a random string created by the generateRandomString function
+// we then add to the urlDatabase object with the key of tempStr and value of the req.body.longURL
+// we then redirect to /urls
 app.post("/urls", (req, res) => {
   let tempStr = generateRandomString();
   urlDatabase[tempStr] = req.body.longURL;
-  let templateVars = { urls : urlDatabase };
   res.redirect("/urls");
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
-  let templateVars = { urls: urlDatabase };
   res.redirect("/urls");
 });
 
 app.post("/urls/:shortURL", (req, res) => {
   urlDatabase[req.params.shortURL] = req.body.longURL;
-  let templateVars = { urls: urlDatabase};
   res.redirect("/urls");
 });
 
@@ -184,14 +205,11 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
-});
-
 app.get("/error", (req, res) => {
   res.status(403).send();
 })
 
 app.listen(PORT, () => {
   console.log('Example app listening on port ${PORT}!');
+  console.log("ooo eee can do!");
 });
